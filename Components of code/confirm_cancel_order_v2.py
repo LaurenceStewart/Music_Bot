b6f241f598c9e449af01777adad287ece85a3727
LaurenceStewart <99291308+LaurenceStewart@users.noreply.github.com>
2022-03-28 10:13:12 +1300

    Finished component 8

--- Components of code/confirm_cancel_order_v2.py
+++ Components of code/confirm_cancel_order_v2.py
@@ -5,16 +5,18 @@ print ("To cancel please enter 2")
 while True:
     try:    
         confirm = int(input("Please enter a number "))
-        if confirm == 1:
-            print("Order Confirmed")
-            break
+        if confirm >= 1 and confirm <= 2:
+            if confirm == 1: 
+                print("Order Confirmed")
+                print("Your order has been sent to our store for processing")
+                break
 
-        elif confirm == 2:
-            print("Order Cancelled")
-            break
-        else: 
-            print("The number must be 1 or 2")
-
+            elif confirm == 2:
+                print("Your Order has been Cancelled")
+                print("You can restart you order or exit the BOT")
+                break
+            else: 
+                print("The number must be 1 or 2")
     except ValueError:
         print ("That is not a valid input")
         print ("Please enter 1 or 2")
